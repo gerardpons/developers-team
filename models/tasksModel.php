2a6723177963f1e91ab7079191a787ec9550f4e0
Gerard Pons <gpguisado@gmail.com>
2022-02-02 11:52:48 +0100

    fixed db path

--- models/tasksModel.php
+++ models/tasksModel.php
@@ -3,7 +3,7 @@
 function getTasks()
 {
     //return $tasks = json_decode(file_get_contents('C:\wamp64\www\todo-project\todo-project\db\tasks.json'), true);
-    return $tasks = json_decode(file_get_contents('./db\tasks.json'), true);
+    return $tasks = json_decode(file_get_contents('./db/tasks.json'), true);
 }
 
 function getTaskById($id)
